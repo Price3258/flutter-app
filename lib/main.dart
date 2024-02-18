570c0606b1f39022e34d0ac379cd114ee766d702
WonTae Kim <rladnjsxo123@naver.com>
2024-02-18 20:14:35 +0900

    feat: 스플래쉬

--- lib/main.dart
+++ lib/main.dart
@@ -1,33 +1,33 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(SplashScreen());
 }
 
-class MyApp extends StatelessWidget {
+class SplashScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        body: SizedBox(
-          height: double.infinity,
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.center,
+        home: Scaffold(
+            body: Container(
+      decoration: BoxDecoration(color: Colors.orange),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Container(height: 50.0, width: 50.0, color: Colors.red),
-              const SizedBox(
-                width: 12.0,
+              Image.asset(
+                'assets/logo.png',
+                width: 200,
               ),
-              Container(height: 50.0, width: 50.0, color: Colors.blue),
-              const SizedBox(
-                width: 12.0,
-              ),
-              Container(height: 50.0, width: 50.0, color: Colors.green),
+              CircularProgressIndicator(
+                valueColor: AlwaysStoppedAnimation(Colors.white),
+              )
             ],
-          ),
-        ),
+          )
+        ],
       ),
-    );
+    )));
   }
 }
